89345a8246bf99fa4436f27a5f75b95a831638d1
jake stec <jakestec99@gmail.com>
2020-10-05 01:58:36 -0500

    init commit

--- scrapers.js
+++ scrapers.js
@@ -5,7 +5,15 @@ async function scrapeProduct(url){
     const browser = await puppeteer.launch({headless: false}); //{headless: false}
     try{
         const page = await browser.newPage();
+        await page.setViewport({width: 1280, height:800});
         await page.goto(url);
+
+        await page.waitFor(2000);
+        
+        await page.evaluate(_ =>{
+            window,scrollBy(0,window.innerHeight);
+        })
+
         let comments = [];
         for (let i = 1; i <= 10; i++){
             await page.waitForXPath(`/html/body/ytd-app/div/ytd-page-manager/ytd-watch-flexy/div[4]/div[1]/div/ytd-comments/ytd-item-section-renderer/div[3]/ytd-comment-thread-renderer[${i}]/ytd-comment-renderer/div[1]/div[2]/ytd-expander/div/yt-formatted-string[2]`);
